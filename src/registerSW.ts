dd210a39de145dd6088c2071375b3eeb6b1a154b
Daniel Birch-Smith <dlbs@outlook.com>
2025-06-22 18:47:50 +0200

    debug: add temp debug messages

--- src/registerSW.ts
+++ src/registerSW.ts
@@ -6,9 +6,11 @@ const updateSW = registerSW({
   onNeedRefresh() {
     // Show a prompt to the user
     console.log('New content available, click on reload button to update.')
+    alert('New content available, click on reload button to update.')
   },
   onOfflineReady() {
     console.log('App ready to work offline')
+    alert('App ready to work offline')
   }
 })
 

6c15c24d24554b8bb0914d55246dad5564a73877
Daniel Birch-Smith <dlbs@outlook.com>
2025-12-10 16:24:50 +0100

    fix: remove popups

--- src/registerSW.ts
+++ src/registerSW.ts
@@ -17,11 +17,11 @@ const {
   onNeedRefresh() {
     // Show a prompt to the user
     console.log('New content available, click on reload button to update.')
-    alert('New content available, click on reload button to update.')
+    // alert('New content available, click on reload button to update.')
   },
   onOfflineReady() {
     console.log('App ready to work offline')
-    alert('App ready to work offline')
+    // alert('App ready to work offline')
   },
   immediate: true
 })
